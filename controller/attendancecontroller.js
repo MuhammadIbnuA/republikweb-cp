0f74ee094a0c56dd100e25a8d6b0762d33c6de17
JundaJagad <121542411+JundaJagad@users.noreply.github.com>
2024-07-08 08:38:42 +0700

    Update attendancecontroller.js

--- controller/attendancecontroller.js
+++ controller/attendancecontroller.js
@@ -1,5 +1,5 @@
 const { db } = require('../firebase');
-const moment = require('moment-timezone');
+const moment = require('moment');
 
 // const checkIn = async (req, res) => {
 //   try {
@@ -99,7 +99,7 @@ const checkIn = async (req, res) => {
   try {
     const { type } = req.body; // type can be 'start', 'resume', 'end', 'break'
     const karyawanId = req.karyawanId; // Ensure karyawanId is correctly extracted from the request
-    const now = moment().tz('Asia/Jakarta');
+    const now = moment();
 
     // Check if karyawanId is defined
     if (!karyawanId) {
