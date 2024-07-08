8432c23ea5239a8452a71da58d5ac03d39216250
JundaJagad <121542411+JundaJagad@users.noreply.github.com>
2024-07-08 07:48:40 +0700

    1

--- controller/attendancecontroller.js
+++ controller/attendancecontroller.js
@@ -99,7 +99,7 @@ const checkIn = async (req, res) => {
   try {
     const { type } = req.body; // type can be 'start', 'resume', 'end', 'break'
     const karyawanId = req.karyawanId; // Ensure karyawanId is correctly extracted from the request
-    const now = moment();
+    const now = moment().tz('Asia/Jakarta'); ;
 
     // Check if karyawanId is defined
     if (!karyawanId) {
