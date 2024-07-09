e45c81b1a98d15221ae202bbbc0ff5e4b9e67e86
JundaJagad <121542411+JundaJagad@users.noreply.github.com>
2024-07-09 16:47:30 +0700

    Update reportcontroller.js

--- controller/reportcontroller.js
+++ controller/reportcontroller.js
@@ -1,11 +1,10 @@
 const { db } = require('../firebase');
 
-// Function to get attendance report for a specific day
 const getDayReport = async (req, res) => {
   try {
     const { date } = req.params;
-    const { fullname } = req.query; // Ambil fullname dari query params
-    
+    const { fullname } = req.query;
+
     const snapshot = await db.collection('attendance')
       .where('date', '==', date)
       .get();
@@ -20,7 +19,6 @@ const getDayReport = async (req, res) => {
     });
 
     if (fullname) {
-      // Filter hasil untuk mencocokkan substring fullname
       report = report.filter(record => 
         record.fullname && record.fullname.toLowerCase().includes(fullname.toLowerCase())
       );
@@ -30,12 +28,15 @@ const getDayReport = async (req, res) => {
       return res.status(404).json({ message: 'No attendance records match the search criteria' });
     }
 
-    res.status(200).json(report);
+    const karyawanIds = report.map(record => record.karyawanId);
+
+    res.status(200).json({ report, karyawanIds });
   } catch (error) {
     console.error('Error retrieving day report:', error);
     res.status(500).json({ message: 'Error retrieving day report', error: error.message });
   }
 };
+
 
 module.exports = {
   getDayReport
